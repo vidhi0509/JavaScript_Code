0759c17d3247282bfaf807721b76ff39987b0758
vidhi0509 <55355053+vidhi0509@users.noreply.github.com>
2023-04-07 21:38:29 +0530

    Add files via upload
    added new project

---
+++ Javascript-practice/Model Window/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+for(let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key); // gives detail of the key pressed
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
